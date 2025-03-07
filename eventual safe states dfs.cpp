16116277bf6af5bc58bad4232bb2fca385221f5d
Soumik Majumder <158304400+SM-GIT-HUB@users.noreply.github.com>
2025-03-08 00:27:23 +0530

    Update eventual safe states dfs.cpp

--- eventual safe states dfs.cpp
+++ eventual safe states dfs.cpp
@@ -1,13 +1,16 @@
+
+// User function Template for C++
+
 class Solution {
-public:
-    bool dfs(int u, vector<int>& visited, vector<int>& ans, vector<vector<int> >& adj)
+  public:
+    bool dfs(int u, vector<int>& visited, vector<int> *adj)
     {
         visited[u] = 2;
         bool flag = 0;
         
         for (int v : adj[u])
         {
-            if (!visited[v] && dfs(v, visited, ans, adj) == true) {
+            if (!visited[v] && dfs(v, visited, adj) == true) {
                 flag = 1;
             }
             else if (visited[v] == 2) {
@@ -21,25 +24,29 @@ public:
         else
         {
             visited[u] = 1;
-            ans.push_back(u);
             return false;
         }
+        
     }
-
-    vector<int> eventualSafeNodes(vector<vector<int> >& adj) {
-        int V = adj.size();
-
+  
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        // code here
         vector<int> visited(V, 0);
         vector<int> ans;
         
         for (int u = 0; u < V; u++)
         {
             if (!visited[u]) {
-                dfs(u, visited, ans, adj);
+                dfs(u, visited, adj);
             }
         }
         
-        sort(ans.begin(), ans.end());
+        for (int node = 0; node < V; node++)
+        {
+            if (visited[node] == 1) {
+                ans.push_back(node);
+            }
+        }
         
         return ans;
     }
